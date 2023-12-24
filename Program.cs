a9c12d57a585d36a40bdf5f525642f4aa390e101
HARIOM-JHA01 <jhahariom040@gmail.com>
2023-12-24 11:06:12 +0530

    Optionaity Changes

--- Program.cs
+++ Program.cs
@@ -9,33 +9,62 @@ class Program
     static void Main()
     {
         // Set the path to the ChromeDriver executable
-        string chromeDriverPath = @"C:\Selenium_project\chromedriver-win64\chromedriver-win64\chromedriver.exe"; // Replace with your actual path
+        string chromeDriverPath = @"C:\Selenium_project\chromedriver-win64\chromedriver-win64\chromedriver.exe";
+
+        //Input url
+        Console.Write("Enter url : ");
+        string url = Console.ReadLine()!;
+
+        //Input Table Info
+        Console.Write("1. Specific Table(Table No.)\n2. All tables\n");
+        Console.Write("Enter Option : ");
+        int tableNo = -1;
+        int choice = int.Parse(Console.ReadLine());
+        if(choice == 1)
+        {
+            Console.Write("Enter Table No. : ");
+            tableNo = int.Parse(Console.ReadLine());
+        } else if(choice == 2)
+        {
+            Console.WriteLine("Working....");
+        } else
+        {
+            Console.WriteLine("Invalid option.");
+            return;
+        }
+        
+
 
         // Initialize the ChromeDriver
-        using (IWebDriver driver = new ChromeDriver(chromeDriverPath))
+        IWebDriver driver = new ChromeDriver(chromeDriverPath);
+        using (driver)
         {
             // Navigate to the website
-            string url = "https://afd.calpoly.edu/web/sample-tables"; // Replace with the actual URL
             driver.Navigate().GoToUrl(url);
-
-            // Uncomment one of the following options based on your preference:
-
-            // Option 1: Extract data from a specific table (e.g., the first table)
-            IWebElement specificTable = driver.FindElement(By.XPath("//table[2]"));
-            List<List<string>> specificTableData = ExtractTableData(specificTable);
-            PrintDataToTerminal(specificTableData, "Specific Table");
-            SaveDataToTextFile(specificTableData, "output_specific_table.txt");
-            SaveDataToCsvFile(specificTableData, "output_specific_table.csv");
-
-            // Option 2: Extract data from all tables on the page
-            // List<IWebElement> allTables = new List<IWebElement>(driver.FindElements(By.TagName("table")));
-            // for (int i = 0; i < allTables.Count; i++)
-            // {
-            //     List<List<string>> tableData = ExtractTableData(allTables[i]);
-            //     PrintDataToTerminal(tableData, $"Table {i + 1}");
-            //     SaveDataToTextFile(tableData, $"output_table_{i + 1}.txt");
-            //     SaveDataToCsvFile(tableData, $"output_table_{i + 1}.csv");
-            // }
+            if (choice == 1)
+            {
+                // Option 1: Extract data from a specific table (e.g., the first table)
+                IWebElement specificTable = driver.FindElement(By.XPath($"//table[{tableNo}]"));
+                List<List<string>> specificTableData = ExtractTableData(specificTable);
+                PrintDataToTerminal(specificTableData, $"Table {tableNo}");
+                SaveDataToTextFile(specificTableData, $"results/output_table_{tableNo}.txt");
+                SaveDataToCsvFile(specificTableData, $"results/output_table_{tableNo}.csv");
+            }
+            else if (choice == 2)
+            {
+                // Option 2: Extract data from all tables on the page
+                 List<IWebElement> allTables = new List<IWebElement>(driver.FindElements(By.TagName("table")));
+                 for (int i = 0; i < allTables.Count; i++)
+                 {
+                     List<List<string>> tableData = ExtractTableData(allTables[i]);
+                     PrintDataToTerminal(tableData, $"Table {i + 1}");
+                     SaveDataToTextFile(tableData, $"results/output_table_{i + 1}.txt");
+                     SaveDataToCsvFile(tableData, $"results/output_table_{i + 1}.csv");
+                 }
+            } else
+            {
+                Console.WriteLine("Invalid Option");
+            }
         }
     }
 
